3f6c7b145bf3fde520c416df9607541b6272cb70
Lee Pham <leenpham@gmail.com>
2024-05-16 12:49:32 -0700

    fixed cancel token error

--- pokedex-react/src/App.js
+++ pokedex-react/src/App.js
@@ -1,51 +1,56 @@
 import React, { useState, useEffect } from "react";
 import PokemonList from "./components/PokemonList";
 import Pagination from "./components/Pagination";
-import axios from "axios"
-
-
+import axios from "axios";
 
 function App() {
-  const [pokemon, setPokemon] = useState([])
-  const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon')
-  const [nextPageUrl, setNextPageUrl] = useState()
-  const [previousPageUrl, setPreviousPageUrl] = useState()
-  const [loading, setLoading] = useState(true)
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [previousPageUrl, setPreviousPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true)
-    let cancel
-    axios.get(currentPageUrl, { cancelToken: new axios.CancelToken(c => cancel=c) }).then(res => {
-    setLoading(false)
-    setNextPageUrl(res.data.next)
-    setPreviousPageUrl(res.data.previous)
-    setPokemon(res.data.results.map(p => p.name))
-    })
+    setLoading(true);
+    const source = axios.CancelToken.source();
 
-    return () => cancel()
+    axios.get(currentPageUrl, { cancelToken: source.token }).then(res => {
+      setLoading(false);
+      setNextPageUrl(res.data.next);
+      setPreviousPageUrl(res.data.previous);
+      setPokemon(res.data.results.map(p => p.name));
+    }).catch(err => {
+      if (axios.isCancel(err)) {
+        console.log('Request canceled', err.message);
+      } else {
+        // handle other errors
+        console.error(err);
+      }
+    });
 
-  }, [currentPageUrl])
+    return () => source.cancel('Operation canceled by the user.');
+
+  }, [currentPageUrl]);
 
   function gotoNextPage() {
-    setCurrentPageUrl(nextPageUrl)
+    setCurrentPageUrl(nextPageUrl);
   }
 
   function gotoPreviousPage() {
-    setCurrentPageUrl(previousPageUrl)
+    setCurrentPageUrl(previousPageUrl);
   }
 
-
   if (loading) {
-    return ("Loading...")
+    return "Loading...";
   }
 
   return (
     <>
-    <PokemonList pokemon={pokemon} />
-    <Pagination
-    gotoNextPage={nextPageUrl ? gotoNextPage: null}
-    gotoPreviousPage={previousPageUrl ? gotoPreviousPage: null}
-    />
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        gotoNextPage={nextPageUrl ? gotoNextPage : null}
+        gotoPreviousPage={previousPageUrl ? gotoPreviousPage : null}
+      />
     </>
   );
 }
