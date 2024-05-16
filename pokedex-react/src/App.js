cea8000b43fbe9e97915018b05a456863fcf855b
Lee Pham <leenpham@gmail.com>
2024-05-15 20:23:19 -0700

    Created a get request to pokeapi

--- pokedex-react/src/App.js
+++ pokedex-react/src/App.js
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PokemonList from "./PokemonList";
+import axios from "axios"
 
 
 
 function App() {
-  const [pokemon, setPokemon] = useState(['bulbasaur', 'charmander'])
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon')
+
+  useEffect(() => {
+  //This is an effect, it's something we want to happen and to rerender it
+  axios.get(currentPageUrl).then(res => {
+    setPokemon(res.data.results.map(p=> p.name))
+  })
+  },[currentPageUrl])
+
+
+
+
+
+
   return (
     // null
     <PokemonList pokemon={pokemon} />
